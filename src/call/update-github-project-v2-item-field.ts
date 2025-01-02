e58219840e026cca34bbd1ba8c30314f25645081
Peter Zhu <zhujiaxi@amazon.com>
2025-01-02 17:19:26 -0500

    Updating initial retrieval of item ids
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- src/call/update-github-project-v2-item-field.ts
+++ src/call/update-github-project-v2-item-field.ts
@@ -12,12 +12,13 @@
 // Arguments    :
 //   - itemId   : (string) the item Node Id when it gets added to a project.
 //   - method   : (string) the method to update an item field in a given project, the item must have been already added to a project.
-//              : label: Adding `Roadmap:Releases/Project Health` label will update the `Roadmap` field value of an item to `Releases, Project Health`.
+//              : - label: Adding `Roadmap:Releases/Project Health` label will update the `Roadmap` field value of an item to `Releases, Project Health`.
+//              :          Field name and value separated by `:` and `/` replaced with ` ,`.
 //   - project  : (string) the `<Organization Name>/<Project Number> where the item field belongs to.
 //              : Ex: `opensearch-project/206` which is the OpenSearch Roadmap Project
 // Requirements : ADDITIONAL_RESOURCE_CONTEXT=true
 
-import { randomBytes } from 'crypto';
+// import { randomBytes } from 'crypto';
 import { Probot } from 'probot';
 import { Resource } from '../service/resource/resource';
 import { validateResourceConfig } from '../utility/verification/verify-resource';
@@ -62,41 +63,43 @@ export default async function UpdateGithubProjectV2ItemField(
     return null;
   }
 
-  const orgName = context.payload.organization.login;
-  const repoName = context.payload.repository.name;
-  const issueNumber = context.payload.issue.number;
-  const issueNodeId = context.payload.issue.node_id;
+  app.log.info(`Start updating fields for project item "${itemId}" ...`)
+
+  //const orgName = context.payload.organization.login;
+  //const repoName = context.payload.repository.name;
+  //const issueNumber = context.payload.issue.number;
+  // const issueNodeId = context.payload.issue.node_id;
 
   // Add to project
-  try {
-    app.log.info(`Attempt to add ${orgName}/${repoName}/${issueNumber} to project ${project}`);
-    const mutationId = await randomBytes(20).toString('hex');
-    const projectSplit = project.split('/');
-    const projectNodeId = resource.organizations.get(projectSplit[0])?.projects.get(Number(projectSplit[1]))?.nodeId;
-    const addToProjectMutation = `
-          mutation {
-            updateProjectV2ItemFieldValue(
-              input: {
-                projectId: "${projectNodeId}",
-                itemId: "${itemId}",
-                fieldId: "${fieldNodeId}",
-                value: {
-                  singleSelectOptionId: "${optionId}"
-                }
-              }
-            ) {
-                projectV2Item {
-                  id
-                }
-              }
-            }
-          `;
-    const responseAddToProject = await context.octokit.graphql(addToProjectMutation);
-    app.log.info(responseAddToProject);
-  } catch (e) {
-    app.log.error(`ERROR: ${e}`);
-    return null;
-  }
+  //try {
+  //  app.log.info(`Attempt to add ${orgName}/${repoName}/${issueNumber} to project ${project}`);
+  //  const mutationId = await randomBytes(20).toString('hex');
+  //  const projectSplit = project.split('/');
+  //  const projectNodeId = resource.organizations.get(projectSplit[0])?.projects.get(Number(projectSplit[1]))?.nodeId;
+  //  const addToProjectMutation = `
+  //        mutation {
+  //          updateProjectV2ItemFieldValue(
+  //            input: {
+  //              projectId: "${projectNodeId}",
+  //              itemId: "${itemId}",
+  //              fieldId: "${fieldNodeId}",
+  //              value: {
+  //                singleSelectOptionId: "${optionId}"
+  //              }
+  //            }
+  //          ) {
+  //              projectV2Item {
+  //                id
+  //              }
+  //            }
+  //          }
+  //        `;
+  //  const responseAddToProject = await context.octokit.graphql(addToProjectMutation);
+  //  app.log.info(responseAddToProject);
+  //} catch (e) {
+  //  app.log.error(`ERROR: ${e}`);
+  //  return null;
+  //}
 
-  return;
+  return null;
 }
