370ec65ec14186192820c277fa717259cae0dd42
Peter Zhu <zhujiaxi@amazon.com>
2025-07-02 14:14:43 -0400

    Bedrock integration
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

---
+++ src/utility/llm/bedrock-client.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+import { BedrockRuntimeClient, ConverseCommand, ConverseCommandInput } from '@aws-sdk/client-bedrock-runtime';
+
+export const bedrockClient = new BedrockRuntimeClient({ region: process.env.AWS_REGION_NAME });
+
+export async function bedrockInvoke(
+  promptText: string,
+  modelId: string = 'arn:aws:bedrock:us-east-1::inference-profile/us.anthropic.claude-3-5-haiku-20241022-v1:0',
+  region: string = 'us-east-1',
+): Promise<string> {
+  const client = bedrockClient;
+
+  const input: ConverseCommandInput = {
+    modelId: modelId,
+    messages: [
+      {
+        role: 'user',
+        content: [{ text: promptText }],
+      },
+    ],
+    inferenceConfig: {
+      maxTokens: 3,
+    },
+  };
+
+  const command = new ConverseCommand(input);
+  const response = await client.send(command);
+  const content = response.output?.message?.content?.[0];
+
+  let textResponse: any;
+
+  if (content && typeof content === 'object' && 'text' in content) {
+    textResponse = content.text;
+  } else {
+    textResponse = 'NO RESPONSE';
+  }
+
+  return textResponse;
+}
